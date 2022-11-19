31ff8035b01bfb4d66376c827524a595ff769c49
Asluax <asluax@gmail.com>
2022-11-20 01:47:05 +0800

    项目初始化

---
+++ gulimall-coupon/src/test/java/com/asluax/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asluax.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
